8528bd38e54edf48353d1a6c0e7519073f7425d1
MikaelFM <89937771+MikaelFM@users.noreply.github.com>
2021-10-26 08:27:41 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
+const spinner = window.document.getElementsByClassName('spinner-border')[0]
+spinner.style.display = 'none'
 const Formatdata = function (x, y) {
-    return String(x) + '/' + String(y + 1) 
+    return String(x) + '/' + String(y + 1)
+}
+function main (){
+    spinner.style.display = 'block';
+    setTimeout(() => {spinner.style.display = 'none';}, 0400);
+    setTimeout(() => {vai()}, 0400);
 }
 function vai() {
     let d = new Date();
@@ -20,34 +27,37 @@ function vai() {
             html += "<div class='dia laranja'><span class='negrito'>" + Formatdata(d.getDate(), d.getMonth()) + "- </span><span>Feriado</span></div>";
         } else {
             contador += 1;
-            if(contador == diasquerer){
+            if (contador == diasquerer) {
                 html += "<div class='dia azul'><span class='negrito'>" + Formatdata(d.getDate(), d.getMonth()) + "- </span><span>" + contador + "º dia  - Entrega</span></div>";
             } else {
                 html += "<div class='dia verde'><span class='negrito'>" + Formatdata(d.getDate(), d.getMonth()) + "- </span><span>" + contador + "º dia</span></div>";
             }
         }
     }
-    if (d.getFullYear() > 2021){
-        alert('Ops... A data de entrega excedeu o ano de 2021. Tente um número mais baixo.' + diferenca() +'no máximo.')
+    if (d.getFullYear() > 2021) {
+        alert('Ops... A data de entrega excedeu o ano de 2021.')
         html = ''
     } else {
         document.getElementById('explicar').innerHTML = html
     }
 }
 document.onkeydown = teclado;
-function teclado(e){
-    if (e.keyCode == 13){
-        vai()
+function teclado(e) {
+    if (e.keyCode == 13) {
+        main()
     }
 }
-function zoom(){
+function zoom() {
     const elemento = window.document.getElementById('id-do-elemento');
     window.document.getElementsByClassName('farrapos')[0].style.transform = 'scale(4)'
 }
-function diferenca(){
-    const past = moment(new Date()); // Data de hoje
-    const now = moment("2021-12-30"); // Outra data no passado
-    const duration = moment.duration(now.diff(past));
-    const days = duration.asDays();
-    return days
-}
+function sleep(milliseconds){
+    var start = new Date().getTime();
+    var end=0;
+    while( (end-start) < milliseconds){
+        end = new Date().getTime();
+    }
+ }
+ function none(){
+     
+ }
